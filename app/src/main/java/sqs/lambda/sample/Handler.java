7e66711cf76c9fd11a8ffb5cfa331b2adf101953
kane8n <takumi.kaneda@zozo.com>
2023-05-22 12:05:19 +0900

    new version test

--- app/src/main/java/sqs/lambda/sample/Handler.java
+++ app/src/main/java/sqs/lambda/sample/Handler.java
@@ -11,6 +11,7 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
 public class Handler implements RequestHandler<SQSEvent, Void> {
     @Override
     public Void handleRequest(SQSEvent event, Context context) {
+        System.out.println("new version: 0518 11:41");
         System.out.println(new String(context.getFunctionVersion()));
         System.out.println(new String(context.getInvokedFunctionArn()));
         for (SQSMessage msg : event.getRecords()) {
